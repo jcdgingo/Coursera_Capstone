fdd94ade3b03b501b259634336ad67cbc6b5b5ce
Jess <jcdgingo@gmail.com>
2020-01-24 15:29:58 +0800

    added prime number determination function

--- python_code.py
+++ python_code.py
@@ -2,4 +2,7 @@ print("Hello Github!")
 def is_prime(x):
     if x == 2:
         return False
-    for 
+    for i in range(sqrt(x), x+1):
+        if x == i:
+            return False
+    return True
